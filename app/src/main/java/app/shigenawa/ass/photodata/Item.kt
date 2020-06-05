151d12e171feecdb57c4462e968a4cd8a9130686
asa-ko <asashige007@gmail.com>
2020-06-05 13:23:13 +0900

    保存可

--- app/src/main/java/app/shigenawa/ass/photodata/Item.kt
+++ app/src/main/java/app/shigenawa/ass/photodata/Item.kt
@@ -1,11 +1,10 @@
 package app.shigenawa.ass.photodata
 
+import android.net.Uri
 import android.widget.ImageView
 import io.realm.RealmObject
 
-
 public open class Item(
+    public open var uri: String?=null
+): RealmObject(){}
 
-    public open var image: ImageView?=null
-
-) : RealmObject(){}
--- app/src/main/java/app/shigenawa/ass/photodata/MainActivity.kt
+++ app/src/main/java/app/shigenawa/ass/photodata/MainActivity.kt
@@ -1,17 +1,20 @@
 package app.shigenawa.ass.photodata
 
-import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
-import android.graphics.BitmapFactory
 import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.net.toFile
+import com.google.android.material.snackbar.Snackbar
 import io.realm.Realm
 import io.realm.RealmConfiguration
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_main.view.*
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
     val realm:Realm=Realm.getDefaultInstance()
@@ -21,16 +24,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+       // Realm.init(this)
+       // val confing=RealmConfiguration.Builder().build()
+       // Realm.setDefaultConfiguration(confing)
+        val item:Item?=read()
+
+        if(item!=null) {
+            val uried:Uri= Uri.parse(item.uri)
+            imageView.setImageURI(uried)
+        }
         val button = findViewById<Button>(R.id.button)
         button.setOnClickListener {
             selectPhoto()
         }
 
-        saveButton.setOnClickListener {
-            val photo: Item? = imageView.
-            val dataText: Context= editText.context
-            save(dataText,photo)
+
+
+     /*   saveButton.setOnClickListener {
+            //ContentResolver
+
+            /*val photouri: Item? =
+         //   val dataText: Context= editText.context
+            save(photo)*/
+
         }
+
+      */
     }
 
     override fun onDestroy() {
@@ -38,20 +57,31 @@ class MainActivity : AppCompatActivity() {
         realm.close()
     }
 
-    /*fun read():{
-        return realm
-    }*/
 
 
-    fun save(context: Context,item: Item){
 
+   fun save(uri: String){
+        val item:Item?=read()
             //保存する処理
             realm.executeTransaction {
-                val save = realm.createObject(Item::class.java)
-                save.image =item.image
+               if (item != null) {
+                    item.uri=uri
+                }else{
+                    val newItem: Item=realm.createObject(Item::class.java)
+                    newItem.uri=uri
+               }
+                Snackbar.make(imageView,"保存しました",Snackbar.LENGTH_SHORT).show()
             }
+       //保存したuriのStringをUriに戻す
+    val newUri:Uri= Uri.parse(uri)
 
     }
+
+
+    fun read():Item?{
+        return realm.where(Item::class.java).findFirst()
+    }
+
 
    /* fun createImageData() : ByteArray {
         val bitmap = (imageView.drawable)
@@ -71,16 +101,35 @@ class MainActivity : AppCompatActivity() {
             READ_REQUEST_CODE -> {
                 try {
                     data?.data?.also { uri ->
+
+                       /* realm.executeTransaction {
+                            val stringUri:String=uri.toString()
+                            //val newItem = realm.createObject(Item::class.java)
+                            //newItem.uri=item.uri
+                            save(stringUri)
+                        }
+                        */
+                        val stringUri:String=uri.toString()
+                        save(stringUri)
+
+
+
                         val inputStream = contentResolver?.openInputStream(uri)
                         val image = BitmapFactory.decodeStream(inputStream)
                         val imageView = findViewById<ImageView>(R.id.imageView)
                         imageView.setImageBitmap(image)
+
+                       //val  stringUri :String=uri.toString()
+                       //save(stringUri)
                     }
+
                 } catch (e: Exception) {
                     Toast.makeText(this, "エラーが発生しました", Toast.LENGTH_LONG).show()
                 }
+
             }
         }
+
     }
 
     private fun selectPhoto() {
